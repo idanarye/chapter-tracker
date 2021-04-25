0bbd9841b87bbb331863662b7a4b803ca26bd693
Idan Arye <IdanArye@gmail.com>
2021-04-25 18:22:08 +0300

    Add revealer for episodes

--- src/gui/series.rs
+++ src/gui/series.rs
@@ -13,7 +13,7 @@ impl actix::Actor for SeriesActor {
     type Context = actix::Context<Self>;
 
     fn started(&mut self, _ctx: &mut Self::Context) {
-        self.widgets.txt_series_name.get_buffer().unwrap().set_text(&self.series.name);
+        self.widgets.txt_series_name.set_text(&self.series.name);
         self.widgets.cbo_media_type.set_active_id(Some(&self.series.media_type.to_string()));
         self.widgets.tgl_unread.set_label(&format!("{}/{}", self.num_unread, self.num_episodes));
     }
@@ -22,9 +22,10 @@ impl actix::Actor for SeriesActor {
 #[derive(woab::WidgetsFromBuilder)]
 pub struct SeriesWidgets {
     pub row_series: gtk::ListBoxRow,
-    pub txt_series_name: gtk::TextView,
+    pub txt_series_name: gtk::Entry,
     pub cbo_media_type: gtk::ComboBox,
     pub tgl_unread: gtk::ToggleButton,
+    rvl_episodes: gtk::Revealer,
 }
 
 impl actix::Handler<woab::Signal> for SeriesActor {
@@ -32,6 +33,11 @@ impl actix::Handler<woab::Signal> for SeriesActor {
 
     fn handle(&mut self, msg: woab::Signal, _ctx: &mut Self::Context) -> Self::Result {
         Ok(match msg.name() {
+            "toggle_episodes" => {
+                let toggle_button: gtk::ToggleButton = msg.param(0)?;
+                self.widgets.rvl_episodes.set_reveal_child(toggle_button.get_active());
+                None
+            }
             _ => msg.cant_handle()?,
         })
     }
