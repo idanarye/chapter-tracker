dac7298209395fa3523d24f00fcc94b2dbb336b8
Idan Arye <IdanArye@gmail.com>
2021-05-05 17:03:07 +0300

    Fix the `run_files_discovery` example again

--- examples/scan_for_new_files.rs
+++ examples/scan_for_new_files.rs
@@ -4,6 +4,6 @@ use sqlx::sqlite::SqlitePool;
 async fn main() -> anyhow::Result<()> {
     flexi_logger::Logger::with_env_or_str("warn").start()?;
     let pool = SqlitePool::connect("sqlite:chapter_tracker.db3").await?;
-    chapter_tracker::files_discovery::run_files_discovery(&pool).await?;
+    chapter_tracker::files_discovery::run_files_discovery(pool.acquire().await?).await?;
     Ok(())
 }
