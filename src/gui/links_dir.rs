545d384bf2af9b92cec7fe56514ee7ea74c76a52
IdanArye <idanarye@gmail.com>
2022-05-03 00:41:15 +0300

    Pad chapter numbers with zeroes

--- src/gui/links_dir.rs
+++ src/gui/links_dir.rs
@@ -43,14 +43,49 @@ impl actix::Handler<crate::gui::msgs::RefreshLinksDirectory> for LinksDirectoryM
                 WHERE episodes.date_of_read IS NULL
                 AND media_types.maintain_symlinks
             "#);
-            let mut unread_episodes: HashMap<_, _> = query.fetch(&mut con).map_ok(|episode: models::Episode| {
-                let base_name = format!("{} {}", episode.name, episode.id);
-                if let Some(extension) = std::path::Path::new(&episode.file).extension() {
-                    (format!("{}.{}", base_name, extension.to_str().unwrap()), episode.file)
-                } else {
-                    (base_name, episode.file)
+            let unread_episodes: Vec::<models::Episode> = query.fetch(&mut con).try_collect().await.unwrap();
+
+            // TODO: Generate names from scratch and get rid of this regex usage...
+            let chapter_pattern = regex::Regex::new(r#"c(\d+)$"#).unwrap();
+
+            let mut pad_series_chapters_to = HashMap::<i64, usize>::new();
+
+            for episode in unread_episodes.iter() {
+                if let Some(chapter) = chapter_pattern.captures(&episode.name).and_then(|m| m.get(1)) {
+                    let length = chapter.as_str().len();
+                    match pad_series_chapters_to.entry(episode.series) {
+                        hashbrown::hash_map::Entry::Occupied(mut entry) => {
+                            let current_max = *entry.get();
+                            *entry.get_mut() = current_max.max(length);
+                        }
+                        hashbrown::hash_map::Entry::Vacant(entry) => {
+                            entry.insert(length);
+                        }
+                    }
                 }
-            }).try_collect().await.unwrap();
+            }
+
+            let mut unread_episodes = unread_episodes.into_iter().map(|episode: models::Episode| {
+                use std::fmt::Write;
+                let mut link_name = if let Some(pad_to) = pad_series_chapters_to.get(&episode.series) {
+                    chapter_pattern.replace(&episode.name, |captures: &regex::Captures| {
+                        let mut result = String::new();
+                        let chapter = captures.get(1).unwrap().as_str();
+                        for _ in chapter.len()..*pad_to {
+                            result.write_char('0').unwrap();
+                        }
+                        result.write_str(chapter).unwrap();
+                        result
+                    }).into_owned()
+                } else {
+                    episode.name
+                };
+                write!(&mut link_name, " {}", episode.id).unwrap();
+                if let Some(extension) = std::path::Path::new(&episode.file).extension() {
+                    write!(&mut link_name, ".{}", extension.to_str().unwrap()).unwrap();
+                }
+                (link_name, episode.file)
+            }).collect::<HashMap<_, _>>();
 
             let read_dir_result = ReadDirStream::new(fs::read_dir(&dir_path).await.unwrap());
             let existing_files: Vec<_> = read_dir_result.try_collect().await.unwrap();
