7f795ee5222d3fe5cdd5d25598800aeff8755f24
Idan Arye <IdanArye@gmail.com>
2023-08-16 16:21:02 +0300

    Add tooltip to media types window

--- .idanarye.moonicipal.lua
+++ .idanarye.moonicipal.lua
@@ -1,22 +1,26 @@
 local moonicipal = require'moonicipal'
 local T = moonicipal.tasks_file()
 
-function T:check()
-    vim.cmd'Erun! cargo check -q --examples'
-end
+T = require'idan.project.rust'(T, {
+    crate_name = 'chapter_tracker',
+})
 
-function T:build()
-    vim.cmd[[
-    botright new
-    terminal cargo build --examples
-    startinsert
-    ]]
-end
+--function T:check()
+    --vim.cmd'Erun! cargo check -q --examples'
+--end
 
-function T:run()
-    vim.cmd[[
-    botright new
-    terminal RUST_BACKTRACE=1 RUST_LOG=chapter_tracker=debug cargo run -- --linksdir episodes-links
-    startinsert
-    ]]
-end
+--function T:build()
+    --vim.cmd[[
+    --botright new
+    --terminal cargo build --examples
+    --startinsert
+    --]]
+--end
+
+--function T:run()
+    --vim.cmd[[
+    --botright new
+    --terminal RUST_BACKTRACE=1 RUST_LOG=chapter_tracker=debug cargo run -- --linksdir episodes-links
+    --startinsert
+    --]]
+--end
